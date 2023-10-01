82cc272e056b3f3cd0c0359ef4184c2eef28b2f7
Njoe00 <nick.james.joe@gmail.com>
2023-09-30 19:04:32 -0700

    adding function that stores each of titles from youtube playlist items into an array

--- src/app/youtube-playlist-titles/page.tsx
+++ src/app/youtube-playlist-titles/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import axios from "axios";
 
@@ -39,40 +38,11 @@ export default function YoutubePlaylistTitles() {
         setPlayListItem(response.data.items);
         console.log("line 26", playListItem[0].snippet.title);
         storeYoutubeTitles();
-=======
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-export default function YoutubePlaylistTitles() {
-  const API_KEY = "AIzaSyDPz_HnRfsgRz708I_83usC0VHIdlVMW9k";
-  const CLIENT_ID =
-    "682640392367-ncgk3k797rt7ihtacadp3kb3h705k5cs.apps.googleusercontent.com";
-  const CLIENT_ID_SECRET = "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";
-  const AUTH_ENDPOINT = "https://www.googleapis.com/auth/youtube.readonly";
-  const REDIRECT_URI = "http://localhost:3000";
-  const RESPONSE_TYPE = "token";
-  const [playListId, setPlayListId] = useState("");
-  const [playListItem, setPlayListItem] = useState<string[]>([]);
-
-  const searchVideos = async (e: any) => {
-    e.preventDefault();
-    await axios
-      .get("https://www.googleapis.com/youtube/v3/search", {
-        params: {
-          part: "snippet",
-          key: API_KEY,
-          q: searchQuery,
-        },
-      })
-      .then((response) => {
-        setVideos(response.data.items);
->>>>>>> d0988e0 (adding mergine conflicts for main file import)
       })
       .catch((error) => {
         console.error("Error fetching YouTube data:", error);
       });
   };
-<<<<<<< HEAD
 
   const urlSpitter = (e: string) => {
     const breakpoint = /\list=/;
@@ -100,15 +70,13 @@ export default function YoutubePlaylistTitles() {
       youtubeTitlesArray.push(playListItem[i].snippet.title);
     }
     return youtubeTitlesArray;
-=======
+  };
   const renderVideos = () => {
     return videos.map((video) => <li key={video.id}>{video.snippet.title}</li>);
->>>>>>> d0988e0 (adding mergine conflicts for main file import)
   };
 
   return (
     <div>
-<<<<<<< HEAD
       <h1>Playlist</h1>
       <form onSubmit={playListItems}>
         <input type="text" onChange={(e) => setPlayListId(e.target.value)} />
@@ -122,14 +90,12 @@ export default function YoutubePlaylistTitles() {
       </form>
       <button onClick={renderPlayListItems}>Click here for titles</button>
       {renderPlayListItems()}
-=======
       <h1>YouTube Videos</h1>
       <form onSubmit={searchVideos}>
         <input type="text" onChange={(e) => setSearchQuery(e.target.value)} />
         <button type={"submit"}>Search</button>
       </form>
       <ul></ul>
->>>>>>> d0988e0 (adding mergine conflicts for main file import)
     </div>
   );
 }
