555588d2996dc9a973f738954e21fe7faa59847a
Njoe00 <nick.james.joe@gmail.com>
2023-10-09 17:15:28 -0700

    fixing merge conflicts

--- src/components/searchAndRenderSongs.tsx
+++ src/components/searchAndRenderSongs.tsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
-=======
-import React, { useState, useEffect } from "react";
->>>>>>> 72c2d8a (adding function to setState for search query by maping array to tracksQuery, then adding a loop to the API call to pass to the stings to the query. Finally, adding a state to track the URIs and adding them the state)
 import axios from "axios";
 
 export default function searchAndRenderSongs({
-<<<<<<< HEAD
-=======
-  setSearchKey,
->>>>>>> 72c2d8a (adding function to setState for search query by maping array to tracksQuery, then adding a loop to the API call to pass to the stings to the query. Finally, adding a state to track the URIs and adding them the state)
   token,
   setTrackUri,
   trackUri,
@@ -24,11 +16,7 @@ export default function searchAndRenderSongs({
 }) {
   useEffect(() => {
     const searchItems = async (songsArray: any) => {
-<<<<<<< HEAD
       songsArray.map(async (songQuery: string, index: number) => {
-=======
-      songsArray.map(async (string: string, index: number) => {
->>>>>>> 72c2d8a (adding function to setState for search query by maping array to tracksQuery, then adding a loop to the API call to pass to the stings to the query. Finally, adding a state to track the URIs and adding them the state)
         try {
           const { data } = await axios.get(
             "https://api.spotify.com/v1/search",
@@ -37,11 +25,7 @@ export default function searchAndRenderSongs({
                 Authorization: `Bearer ${token}`,
               },
               params: {
-<<<<<<< HEAD
                 q: songQuery,
-=======
-                q: string,
->>>>>>> 72c2d8a (adding function to setState for search query by maping array to tracksQuery, then adding a loop to the API call to pass to the stings to the query. Finally, adding a state to track the URIs and adding them the state)
                 type: "track",
               },
             }
@@ -54,49 +38,15 @@ export default function searchAndRenderSongs({
             ...trackUri,
             data.tracks.items[index].uri,
           ]);
-<<<<<<< HEAD
-=======
-          console.log(trackUri);
->>>>>>> 72c2d8a (adding function to setState for search query by maping array to tracksQuery, then adding a loop to the API call to pass to the stings to the query. Finally, adding a state to track the URIs and adding them the state)
           console.log("song query succesful:", data);
         } catch (error) {
           console.error("Error finding tracks:", error);
         }
       });
-<<<<<<< HEAD
       console.log(trackUri);
-=======
->>>>>>> 72c2d8a (adding function to setState for search query by maping array to tracksQuery, then adding a loop to the API call to pass to the stings to the query. Finally, adding a state to track the URIs and adding them the state)
     };
     searchItems(songsArray);
   }, [tracksQuery]);
 
-<<<<<<< HEAD
   return <div></div>;
-=======
-  // const renderTracks = () => {
-  //   return itemSearch.map((data: spotifyTracksObj, id: number) => (
-  //     <div className="text-orange-600 text-lg" key={id}>
-  //       {data ? (
-  //         <>
-  //           <img alt="" width={"25%"} src={data.album.images[0].url} />
-  //           {data.name}
-  //         </>
-  //       ) : (
-  //         <div> "No Songs Available"</div>
-  //       )}
-  //     </div>
-  //   ));
-  // };
-  return (
-    <div>
-      {/* <button onClick={}>Click me to pass title arrays</button> */}
-      {/* <form onSubmit={searchItems}>
-        <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
-        <button type={"submit"}>search tracks</button>
-      </form> */}
-      {/* {renderTracks()} */}
-    </div>
-  );
->>>>>>> 72c2d8a (adding function to setState for search query by maping array to tracksQuery, then adding a loop to the API call to pass to the stings to the query. Finally, adding a state to track the URIs and adding them the state)
 }
