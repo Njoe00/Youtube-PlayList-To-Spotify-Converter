3fd6de88a03c7a79c93dc70f73436991c1e5d355
Njoe00 <nick.james.joe@gmail.com>
2023-10-25 13:11:47 -0700

    adding show dialog to have user sign in everytime and adding button that matches spotify style

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -12,16 +12,16 @@ export default function LoginCard({
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
   const SCOPE = "playlist-modify-private playlist-modify-public";
-  const SHOW_DIALOG = false;
+  const SHOW_DIALOG = "true";
   const [email, setEmail] = useState("");
 
   return (
-    <div className="flex justify-center pt-10 flex-col">
-      <div className="w-[600px] h-[600px] flex justify-center text-orange-400">
-        <div className="">
+    <div className="flex justify-center flex-row">
+      <div className="py-[14px] px-[20px] bg-spotfiy-colors flex justify-center text-black rounded-full">
+        <div>
           {!token ? (
             <a
-              href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&show_dialog${SHOW_DIALOG}`}
+              href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&show_dialog=${SHOW_DIALOG}`}
             >
               Login to Spotify
             </a>
@@ -29,13 +29,6 @@ export default function LoginCard({
             <button onClick={logout}>Logout</button>
           )}
         </div>
-        {/* <div className="h-12 w-[400px] absolute flex">
-          <input
-            value={email}
-            placeholder="Enter your email here"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div> */}
       </div>
     </div>
   );
