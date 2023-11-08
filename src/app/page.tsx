a9dd44c321e008a03a898434ba0b1305683ffd0e
Njoe00 <nick.james.joe@gmail.com>
2023-11-08 09:39:19 -0800

    fixing naming issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
               />
             ) : (
               <Playlist
-                token={token1}
+                token1={token1}
                 setSpotifyPlayListId={setSpotifyPlayListId}
               />
             )}
