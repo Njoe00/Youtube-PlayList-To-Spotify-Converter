a49e2afeb5cfa6881a200a452c991e9cb99492e7
Njoe00 <nick.james.joe@gmail.com>
2023-11-09 18:26:58 -0800

    testing wave form import to see if its causing the issues

--- src/components/youtubeplaylist.tsx
+++ src/components/youtubeplaylist.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import axios from "axios";
-import { waveform } from "ldrs";
-waveform.register();
+// import { waveform } from "ldrs";
+// waveform.register();
 
 import { playListItemObj } from "../app/page";
 
@@ -86,12 +86,12 @@ export default function YoutubePlaylistTitles({
         )}
         {isLoading && (
           <div className="m-8">
-            <l-waveform
+            {/* <l-waveform
               size="50"
               stroke="4"
               speed="2"
               color="#16163F"
-            ></l-waveform>
+            ></l-waveform> */}
           </div>
         )}
       </div>
